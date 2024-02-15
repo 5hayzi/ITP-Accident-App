24b955a4ebf556b8b1b0fedd7bf25a97c78d29cc
Arsalan Javed <arsalan.javedawan@gmail.com>
2024-02-15 22:30:15 +0500

    Made a sargeant info screen and some other stuff

---
+++ app/src/main/java/com/example/accidentreportsitp/sergeantInfo.java
@@ -0,0 +1,80 @@
+package com.example.accidentreportsitp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class sergeantInfo extends AppCompatActivity {
+
+    TextView showdate,showtime;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sergeant_info);
+
+        showdate = findViewById(R.id.date);
+        showtime = findViewById(R.id.time);
+
+        showdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                openDateDialog();
+            }
+        });
+
+        showtime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openTimeDialog();
+            }
+        });
+
+
+    }
+
+    public void openDateDialog(){
+        DatePickerDialog dialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                showdate.setText(String.valueOf(year) + "\\" + String.valueOf(month) + "\\" + String.valueOf(year));
+            }
+        },2023,1,14);
+        dialog.show();
+    }
+
+    public void openTimeDialog() {
+        // Get current time
+        int hour = 15;
+        int minute = 0;
+
+        // Create a TimePickerDialog
+        TimePickerDialog dialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        // Update the TextView with the selected time
+                        showtime.setText(String.format("%02d:%02d", hourOfDay, minute));
+                    }
+                }, hour, minute, true);
+
+        // Show the TimePickerDialog
+        dialog.show();
+    }
+
+
+
+
+
+
+}
