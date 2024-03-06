c16ca29127751630e73e397f54be44f19846128d
Sheraz Ahmed <hithisissheraz1947@gmail.com>
2024-03-06 20:08:25 +0500

    code improvements, strings-en.xml with translation, new java functionalities

--- app/src/main/java/com/example/accidentreportsitp/splashScreen.java
+++ app/src/main/java/com/example/accidentreportsitp/splashScreen.java
@@ -1,10 +1,10 @@
 package com.example.accidentreportsitp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+
 
 public class splashScreen extends AppCompatActivity {
 
@@ -15,14 +15,19 @@ public class splashScreen extends AppCompatActivity {
 
 
 
+        // Delayed handler to start the next activity after 2500 milliseconds
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 // Start the next activity
-                Intent intent = new Intent(splashScreen.this, Death_screen.class);
+                Intent intent = new Intent(splashScreen.this, sergeantInfo.class);
                 startActivity(intent);
                 finish(); // Finish the splash activity
             }
         }, 2500);
+
+        // Button click listener to switch language
+
     }
+
 }
